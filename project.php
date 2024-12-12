3fc9ceab8c199612c65bc7d90dde91feadc4fdd2
Serra Arnold <sermarno@iu.edu>
2024-12-11 21:24:54 -0500

    Started nav bar

--- project.php
+++ project.php
@@ -3,9 +3,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Project Overview</title>
+    <!-- Linking CSS Stylesheet -->
+    <link rel="stylesheet" href="css/styles.css">
 </head>
 <body>
+    <main>
+        <!-- Nav Bar -->
+        <div class="nav">
+            <h3><a href="index.php">Website Title</a></h3>
+            <?php include 'includes/nav.php' ?>
+        </div>
+
+    </main>
     
 </body>
 </html>
