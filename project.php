1f25933f1b0b2b31cff845b33527ad4c26c1c8ee
Serra Arnold <sermarno@iu.edu>
2024-12-15 14:49:50 -0500

    Filling in placeholder text

--- project.php
+++ project.php
@@ -21,33 +21,58 @@
         <div class="scroll">
             <h3>Project Overview:</h3>
             <ul> 
-                <li><a href="#problem">The Problem</a></li>
-                <li><a href="#goal">Our Goal</a></li>
-                <li><a href="#stakehoders">Stakeholders</a></li>
-                <li><a href="#features">Features & Benefits</a></li>
+                <li><a href="#problem">The Problem ↓</a></li>
+                <li><a href="#goal">Our Goal ↓</a></li>
+                <li><a href="#stakehoders">Stakeholders ↓</a></li>
+                <li><a href="#features">Features & Benefits ↓</a></li>
             </ul>
         </div>
 
         <article id="problem">
             <h3>The Problem</h3>
             <p>
-                Here will be a section that discusses the problem that we 
-                are developing a solution for.
+                Aldeas Inteligentes, Bienestar Sostenible is a Mexican Federal 
+                Government project actively working to provide digital access 
+                to rural and isolated communities across Mexico. Having already 
+                connected 83 communities, this project provides community residents 
+                with a range of wireless internet connection methods, STEM training, 
+                and support in community development projects aimed towards improving 
+                education, commerce, health, welfare, and other developmental aspects. 
+                <br><br>
+                Currently, administrators rely on project updates through a questionnaire 
+                completed by residents once every three months, but with long gaps between 
+                check-ins and no effective communication tool, it is challenging to maintain 
+                contact with the residents and ensure that the projects are receiving the 
+                attention they need for their completion. If there isn’t an effective tool 
+                to frequently track the status of community projects and reach out to community 
+                members, residents miss opportunities to gain recognition for their work, inspire 
+                greater involvement, and secure additional funding or support for their community 
+                development.
             </p>
         </article>
         <article id="goal">
             <h3>Our Goal</h3>
             <p>
-                Here will be a section that reveals the goal of our project
-                and how it will help the government of Mexico's current operations
-                on their project.
+                Our goal as a team is to create an information system that manages 
+                Smart Village operations, enabling communities to showcase their projects on an 
+                official website and to stay in contact with curators for assistance, encouraging 
+                greater participation and ultimately increasing their potential for further development. 
+                Including a visualization of community projects enhances transparency through a clear way 
+                for the public to see what projects are being worked on through the provided technology
+                and assistance. By providing administrators and residents a communication tool directly through
+                the website, users can get immediate responses to their concerns or questions.
             </p>
         </article>
         <article id="stakeholders">
             <h3>Stakeholders</h3>
-            <p>
-                Here will be a section that lists stakeholders.
-            </p>
+            <ul>
+                <li>Rural Community Residents - these individuals are the focus group for this
+                    project.</li>
+                <li>Smart Village Support Staff - this includes administrators, developers, and 
+                    any other individuals who work behind the scenes to support the residents.</li>
+                <li>Site Visitors - this includes any visitors of the website who are looking at what
+                    the site has to offer.</li>
+            </ul>
         </article>
         <article id="features">
             <h3>Features and Benefits</h3>
