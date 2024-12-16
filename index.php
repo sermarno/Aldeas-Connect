9a348be04ab8c5412aba8b6a35dd21d38f6e386e
Nosadeba Imasuen <nimasuen@iu.edu>
2024-12-15 19:10:30 -0500

    Cleaning The Design

--- index.php
+++ index.php
@@ -4,15 +4,28 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Landing Page</title>
+    <!-- Linking CSS Stylesheet -->
+    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="css/normalize.css">
 </head>
 <body>
-    <h1>Home Page</h1>
-    <a href="./index.php">Home Page</a>
-    <a href="./project.php">About the Project</a>
-    <a href="./team.php">About Us</a>
-    <p>Aldeas Inteligentes is a transformative initiative by the Mexican Federal Government aimed at providing digital access to rural and isolated communities across Mexico. 
-    By connecting 83 communities with wireless internet, offering STEM training, and supporting community development projects, Aldeas Inteligentes is enhancing education, commerce, health, and overall welfare.
-    Our information system will change how rural communities track progress, showcase their achievements, and connect with supporters.
-    From improving education and healthcare to boosting local commerce, we're creating a platform that helps amplify the voices and aspirations of Mexico's underrated regions.</p>
+    <header>
+        <h1>Project Overview: <br><span>La Conexión y el Futuro de las Aldeas Inteligentes</span></h1>
+    </header>
+
+    <!-- Nav Bar -->
+    <div class="nav">
+            <h3><a href="index.php">Home</a></h3>
+            <?php include 'includes/nav.php' ?>
+    </div>
+    <article id="overview">
+        <h3>Overview</h3>
+        <p>
+            Aldeas Inteligentes is a transformative initiative by the Mexican Federal Government aimed at providing digital access to rural and isolated communities across Mexico. 
+            By connecting 83 communities with wireless internet, offering STEM training, and supporting community development projects, Aldeas Inteligentes is enhancing education, commerce, health, and overall welfare.
+            Our information system will change how rural communities track progress, showcase their achievements, and connect with supporters.
+            From improving education and healthcare to boosting local commerce, we're creating a platform that helps amplify the voices and aspirations of Mexico's underrated regions.
+        </p>
+    </article>
 </body>
 </html>
