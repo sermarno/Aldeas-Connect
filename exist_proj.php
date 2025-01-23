6451a5df7a1f9adcc670b333975886d886a55046
Serra Arnold <sermarno@iu.edu>
2025-01-23 13:24:48 -0500

    Adding request button on home page and existing/new project request files

---
+++ exist_proj.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Edit/Remove Project Request</title>
+    <!-- Linking CSS Stylesheet -->
+    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="css/normalize.css">
+</head>
+<body>
+    <!-- Nav Bar -->
+    <div class="nav">
+            <h3><a href="index.php">Home</a></h3>
+            <?php include 'includes/nav.php' ?>
+    </div>
+    <header>
+        <h1>Existing Project Request Form</h1>
+    </header>
+</body>
+</html>
