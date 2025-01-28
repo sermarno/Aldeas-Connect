69e9ffb8a312626adfcd10d32055eb01b0d2d4c2
Serra Arnold <sermarno@iu.edu>
2025-01-28 16:33:10 -0500

    Fixed investor link in nav

--- includes/nav.php
+++ includes/nav.php
@@ -3,6 +3,6 @@
         <li><a href="index.php">Home</a></li>
         <li><a href="team.php">Team</a></li>
         <li><a href="project.php">Project</a></li>
-        <li><a href="invesor.php">Investors</a></li>
+        <li><a href="investor.php">Investors</a></li>
     </ul>
 </nav>
