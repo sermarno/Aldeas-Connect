e06a83c4dbca70a52348d7d82b945df40a8c89a2
Serra Arnold <sermarno@iu.edu>
2025-02-06 19:48:34 -0500

    Publishing gallery file and adding to nav bar

--- includes/nav.php
+++ includes/nav.php
@@ -5,5 +5,6 @@
         <li><a href="project.php">Project</a></li>
         <li><a href="investor.php">Investors</a></li>
         <li><a href="login.php">Login</a></li>
+        <li><a href="gallery.php">Gallery</a></li>
     </ul>
 </nav>
