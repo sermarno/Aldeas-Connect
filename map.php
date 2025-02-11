b7f38651aa82182d6f2d7fe0c017e8365e22e57e
ecristan <ecristan@iu.edu>
2025-02-11 14:25:38 -0500

    Adding Coordinates

--- map.php
+++ map.php
@@ -3,22 +3,64 @@
 //array of village data placeholders still need to pull info to village
 $villages = [
     [
-        "name" => "Village A",
-        "lat" => 19.4326,
-        "lng" => -99.1332,
-        "info" => "This is Village A. Known for its agricultural projects."
+        "name" => "Sacún Cubwitz",
+        "lat" => 17.1275,
+        "lng" => -91.93989,
+        "info" => "Centro de Salud in Chiapas, focused on processing and sharing health sector reports."
     ],
     [
-        "name" => "Village B",
-        "lat" => 20.6597,
-        "lng" => -103.3496,
-        "info" => "This is Village B. Famous for its STEM initiatives."
+        "name" => "Siberia",
+        "lat" => 16.61759,
+        "lng" => -92.29259,
+        "info" => "Centro de Salud Microregional in Siberia, Chiapas, improving healthcare report sharing."
     ],
     [
-        "name" => "Village C",
-        "lat" => 21.1619,
-        "lng" => -86.8515,
-        "info" => "This is Village C. Focused on e-commerce development."
+        "name" => "Yibeljoj",
+        "lat" => 16.95799,
+        "lng" => -92.504199,
+        "info" => "Centro de Salud Microregional in Yibeljoj, working on better jurisdictional health coordination."
+    ],
+    [
+        "name" => "Nueva Tenochtitlán",
+        "lat" => 16.4769,
+        "lng" => -94.08289,
+        "info" => "Centro de Salud Nueva Tenochtitlán, focusing on digital health records and remote care."
+    ],
+    [
+        "name" => "Gabriel Leyva Velázquez",
+        "lat" => 16.4614,
+        "lng" => -91.835999,
+        "info" => "Centro de Salud in Las Margaritas, enhancing health information sharing."
+    ],
+    [
+        "name" => "San Antonio Las Delicias",
+        "lat" => 16.8869,
+        "lng" => -91.8763,
+        "info" => "Centro de Salud providing support in remote medical reporting."
+    ],
+    [
+        "name" => "Roberto Barrios",
+        "lat" => 17.3255,
+        "lng" => -91.9257,
+        "info" => "Healthcare initiative in Palenque, Chiapas."
+    ],
+    [
+        "name" => "Mariano Matamoros",
+        "lat" => 16.48079,
+        "lng" => -92.5849,
+        "info" => "Centro de Salud supporting health jurisdiction reports."
+    ],
+    [
+        "name" => "Nuevo Limar",
+        "lat" => 17.45329,
+        "lng" => -92.3957,
+        "info" => "Health center working on jurisdiction and hospital coordination."
+    ],
+    [
+        "name" => "Esperanza de los Pobres",
+        "lat" => 17.30969,
+        "lng" => -93.4825,
+        "info" => "Centro de Salud promoting data sharing in Chiapas."
     ],
 ];
 ?>
