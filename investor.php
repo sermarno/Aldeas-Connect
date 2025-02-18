14a0d0d26b0cc61c894ef413e9da7f6b2e0da4f8
Nosadeba Imasuen <nimasuen@iu.edu>
2025-02-17 23:25:13 -0500

    More Cleanup in investor

--- investor.php
+++ investor.php
@@ -1,26 +1,3 @@
-<?php
-    include "includes/db.php";
-    // Database Query
-    $query = "SELECT * FROM required_help";
-    $result_set = mysqli_query($conn, $query);
-    if ($result_set){
-        echo "<h2>Required Help</h2>";
-        echo "<table border='1'>";
-        echo "<tr><th>Community</th><th>Required Resources</th></tr>";
-
-        while ($row = mysqli_fetch_assoc($result)) {
-            echo "<tr>";
-            echo "<td>" . $row['community'] . "</td>";
-            echo "<td>" . $row['req_resources'] . "</td>";
-            echo "</tr>";
-        }
-        echo "</table>";
-    } else {
-        echo "No dish records found.<br>";
-    }
-    // Close the Connection
-    mysqli_close($conn)
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -31,6 +8,31 @@
     <link rel="stylesheet" href="css/styles.css">
     <link rel="stylesheet" href="css/normalize.css">
 </head>
+
+<?php
+    include "includes/db.php";
+    // Database Query
+    $query = "SELECT * FROM required_help";
+    $result_set = mysqli_query($conn, $query);
+    if ($result_set){
+        echo "<h2>Required Help</h2>";
+        echo "<table border='1'>";
+        echo "<tr><th>Community</th><th>Required Resources</th></tr>";
+
+        while ($row = mysqli_fetch_assoc($result_set)) {
+            echo "<tr>";
+            echo "<td>" . $row['community'] . "</td>";
+            echo "<td>" . $row['req_resources'] . "</td>";
+            echo "</tr>";
+        }
+        echo "</table>";
+    } else {
+        echo "No dish records found.<br>";
+    }
+    // Close the Connection
+    mysqli_close($conn);
+?>
+
 <body>
     <!-- Nav Bar -->
     <div class="nav">
@@ -47,39 +49,11 @@
 
 <!--Table to show ongoing projects, projects in progress, etc.-->
 
-<table>
-    <tr>
-        <th>Title</th>
-        <th>Description</th>
-        <th>Project Timeline</th>
-        <th>Project Percentage</th>
-    </tr>
-</table>
+
 <h2>Where You Can Help</h2>
 <!--Table to show project title, where the project needs assistance.-->
 <?php include 'includes/footer.php' ?>
-<table>
-    <tr>
-        <th>Community</th>
-        <th>Required Resources</th>
-    </tr>
-    <tr>
-        <td>Yokdzonot-Hu, Yaxkabá</td>
-        <td>More carving tools.</td>
-    </tr>
-    <tr>
-        <td>Tikum, Tekax</td>
-        <td>More containers.</td>
-    </tr>
-    <tr>
-        <td>Hunukú, Temozón</td>
-        <td>More computers for online resources.</td>
-    </tr>
-    <tr>
-        <td>Cazumá, Cazumá</td>
-        <td>More wifi routers for intenet.</td>
-    </tr>
-</table>
+
 
 </body>
 </html>
