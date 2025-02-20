7ea194d1e9c429ec4237e9b608a0c13ccf06c34a
Serra Arnold <sermarno@iu.edu>
2025-02-20 15:53:02 -0500

    Removing duplicate files

--- includes/footer 4.php
+++
@@ -1,9 +0,0 @@
-<footer>
-    <h5><a href="index.php">Aldeas Inteligentes IU</a></h5>
-    <ul>
-        <li><a href="team.php">Meet the Team</a></li>
-        <li><a href="project.php">Our Purpose</a></li>
-        <li><a href="investor.php">See How You Can Help</a></li>
-        <li><a href="gallery.php">Photo Gallery</a></li>
-    </ul>
-</footer>
