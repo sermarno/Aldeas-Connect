2dfa014297ac23ccbc9c87f13d30629af48372a6
Serra Arnold <sermarno@iu.edu>
2025-02-20 20:29:09 -0500

    Styling and rearranging navigation for better look

--- includes/nav.php
+++ includes/nav.php
@@ -1,13 +1,15 @@
 <nav>
     <ul>
-        <li><?php include "includes/side_nav.php" ?></li>
-        <a href="index.php">
-            <img src="img/logo.png" alt="home">
-        </a>
+        <li class="logo">
+            <a href="index.php">
+                <img src="img/logo.png" alt="home">
+            </a>
+        </li>
         <li class="login">
             <a href="login.php">
                 <img src="img/account.png" alt="account"></img>
             </a>
         </li>
+        <li><?php include "includes/side_nav.php" ?></li>
     </ul>
 </nav>
