fdd69daad3fcbbf6a75dcb18c93cebfe45abd28c
Serra Arnold <sermarno@iu.edu>
2025-02-20 22:04:46 -0500

    Adding styling and layout changes to index

--- includes/nav.php
+++ includes/nav.php
@@ -5,6 +5,10 @@
                 <img src="img/logo.png" alt="home">
             </a>
         </li>
+        <li>
+            <button onclick="translatePage()">Translate to Spanish</button>
+            <div id="google_translate_element" style="display:none;"></div>
+        </li>
         <li class="login">
             <a href="login.php">
                 <img src="img/account.png" alt="account"></img>
