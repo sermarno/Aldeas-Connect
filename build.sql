b5a163e2fe928d7ed74889564f0b6be9295d3fdd
Serra Arnold <sermarno@iu.edu>
2025-02-24 16:39:25 -0500

    Register/Login storing users in database with google api

--- build.sql
+++ build.sql
@@ -1,31 +1,18 @@
-DROP TABLE IF EXISTS messages CASCADE;
-DROP TABLE IF EXISTS project_requests CASCADE;
-DROP TABLE IF EXISTS projects CASCADE;
 DROP TABLE IF EXISTS required_help;
-DROP TABLE IF EXISTS users CASCADE;
-DROP TABLE IF EXISTS communities CASCADE;
-DROP TABLE IF EXISTS projects CASCADE;
-DROP TABLE IF EXISTS project_requests CASCADE;
 DROP TABLE IF EXISTS testimonials CASCADE;
+DROP TABLE IF EXISTS project_requests CASCADE;
+DROP TABLE IF EXISTS projects CASCADE;
+DROP TABLE IF EXISTS communities CASCADE;
+DROP TABLE IF EXISTS users CASCADE;
 
 -- user
 CREATE TABLE users (
     user_id INT PRIMARY KEY AUTO_INCREMENT,
-    full_name VARCHAR(255),
+    fname VARCHAR(255),
+    lname VARCHAR(255),
     username VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL,
-    user_password VARCHAR(255) NOT NULL,
+    email VARCHAR(255),
     user_role ENUM('resident', 'admin', 'visitor')
-) ENGINE=INNODB;
-
--- messages 
-CREATE TABLE messages (
-    message_id INT PRIMARY KEY AUTO_INCREMENT,
-    message_text TEXT NOT NULL,
-    sender_id INT NOT NULL,
-    receiver_id INT NOT NULL,
-    FOREIGN KEY (sender_id) REFERENCES users(user_id),
-    FOREIGN KEY (receiver_id) REFERENCES users(user_id)
 ) ENGINE=INNODB;
 
 -- community
@@ -68,7 +55,6 @@ CREATE TABLE project_requests (
 ) ENGINE=INNODB;
 
 -- Testimonial/Connectivity
--- Tetimonial/Connectivity
 CREATE TABLE testimonials (
     testimonial_id INT PRIMARY KEY AUTO_INCREMENT,
     user_id INT NOT NULL,
@@ -92,9 +78,8 @@ CREATE TABLE required_help (
 ) ENGINE=INNODB;
 
 -- insert statements with test data
-INSERT INTO users (full_name, username, email, user_password, user_role) VALUES
-('User 1', 'user1', 'user1@example.com', 'password1', 'resident'),
-('User 2', 'user2', 'user2@example.com', 'password2', 'admin');
+INSERT INTO users (fname, lname, username, email, user_role) VALUES
+('fname', 'lname', 'user1', 'user1@example.com', 'resident');
 
 INSERT INTO communities (comm_name, comm_description, comm_location) VALUES
 ('Community 1', 'Community 1 description', 'Community 1 location'), 
@@ -110,9 +95,6 @@ INSERT INTO project_requests (title, proj_description, proj_start, proj_end, req
 ('Project 3', 'Project 3 description', '2025-04-09', '2026-01-03', 'pending', null, 1, 1),
 ('Project 4', 'Project 4 description', '2025-11-09', '2028-11-04', 'approved', "This is a really great project to work on!", 2, 2);
 
-INSERT INTO project_requests (title, req_description, proj_start, proj_end, res_comments, req_status, user_id, community_id) VALUES
-('Project Req 1', 'Project Req 1 description', '2022-11-09', '2026-01-03', 'Resident Comments 1', 'pending', 1, 1);
-
 INSERT INTO testimonials (user_id, community_id, story_text, video_url, category, status)
 VALUES
 (1, 1, 'My name is Elisa Cercanche, I live in the community of Tiunca, municipality of Yaxcabá, I am a community educator in the community of San Marcos. Preschool level, for me the smart villages project is one, it is a support that has served us in the Community, in rural communities such as the Community of Tiuncá, since it has been used educationally.', 'uploads/sample_video.mp4', 'Education', 'approved'),
@@ -125,7 +107,4 @@ INSERT INTO required_help (community, req_resources) VALUES
 ('Hunukú, Temozón', 'More computers for online resources.'),
 ('Cazumá, Cazumá', 'More wifi routers for intenet.');
 
-INSERT INTO messages (message_text, sender_id, receiver_id) VALUES
-("hello", 2, 1),
-("hi", 1, 2);
 
