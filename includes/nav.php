e3281464887ff5faa193527431515d69cb96fec9
Serra Arnold <sermarno@iu.edu>
2025-03-10 12:21:31 -0400

    Making requests icon be in main nav when user role is resident/admin

--- includes/nav.php
+++ includes/nav.php
@@ -9,13 +9,29 @@
             <button onclick="translatePage()">Translate to Spanish</button>
             <div id="google_translate_element" style="display:none;"></div>
         </li>
+
+        <?php 
+        session_start();
+
+        // Only show request icon if user is logged in AND role is 'resident' or 'admin'
+        if (isset($_SESSION['user_role']) && ($_SESSION['user_role'] === 'resident' || $_SESSION['user_role'] === 'admin')) {
+            echo '<li class="account-dropdown">
+                    <a href="requests.php">
+                        <img class="request_icon" src="img/request_icon.png" alt="request">
+                    </a>
+                    <div class="dropdown-content">
+                        <a href="requests.php">Requests</a>
+                    </div>
+                  </li>';
+        }
+        ?>
+
         <li class="account-dropdown">
             <a href="javascript:void(0)">
-                <img src="img/account.png" alt="account"></img>
+                <img class='account-icon' src="img/account.png" alt="account">
             </a>
             <div class="dropdown-content">
                 <?php
-                session_start();
                 if (isset($_SESSION['user_role'])) {
                     echo '<a href="account.php">Account Details</a>';
                     echo '<a href="logout.php">Logout</a>';
--- includes/side_nav.php
+++ includes/side_nav.php
@@ -1,16 +1,6 @@
 <nav id="side_nav" class="side_nav">
     <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
     <ul>
-        <?php 
-        session_start();
-
-        // making certain links only accesible to admin/residents
-        if (isset($_SESSION['user_role'])) {
-            if ($_SESSION['user_role'] === 'resident' || $_SESSION['user_role'] === 'admin') {
-                echo '<li><a href="requests.php">Project Requests</a></li>';
-            }
-        }
-        ?>
         <li><a href="investor.php">Investors</a></li>
         <li><a href="gallery.php">Gallery</a></li>
         <li><a href="success_stories.php">Success Stories</a></li>
